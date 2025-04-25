4cbd4c131dd121108129f3a16ca36df24ac8af14
jarodriguez37 <jarodriguez37@espe.edu.ec>
2025-04-25 08:00:52 -0500

    first commit

---
+++ PRACTICA.cpp
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+
+
+
+
+
+
+
+
+
+
+int main(){
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
